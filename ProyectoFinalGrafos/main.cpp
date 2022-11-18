b14c654521d7ea0f3c75ad18899d38df1ed56e7f
Angel Ramirez Castro <92240740+AngelRamirez02@users.noreply.github.com>
2022-11-17 23:25:27 -0600

    Update main.cpp

--- ProyectoFinalGrafos/main.cpp
+++ ProyectoFinalGrafos/main.cpp
@@ -2,15 +2,47 @@
 #include"Listag.h"
 using namespace std;
 
+bool validaNum(char datoUser[]){
+	int val, cont, comp;
+	string aux = datoUser;
+	comp=aux.length();//tamaño del recorrido del ciclo
+	char letra;
+	int init = 48, fin=57;//rango de valores decimales donde se encuentran los numero 0-9
+	
+	for(int i=0; i<=comp; i++){
+		letra = datoUser[i];
+		val = letra;
+		for(int j=init; j<=fin; j++){
+			if(val==j){
+				cont++;
+			}
+		}
+	}
+	if(comp == cont){
+		return true;
+	}else{
+		cout<<"\n\t\tPOR FAVOR INGRESE UN NUMERO ENTERO"<<endl;
+		return false;
+	}
+}
+
+
 int main(){
 lista l;
 int opc,opc2,inicio,final,peso,vertice;
-char cont;
+char cont, opUser[5];
 do{
-system("cls");cout<<"\nMENU:\n\n1- Utilizar Grafo Pre-guardado\n2- Crear Grafo\n3- Crear Matriz de Adyacencia\n4- Crear Matriz de Caminos\n5- Mostar Lista de Nodos Adyacencentes\n6- Recorrido En Profundidad\n7- Recorrido En Anchura\n8- Mostar Matriz de Floyd\n9- Mostar Matriz de Warshall\n10- Salir\n\n Ingrese la opcion: ";cin>>opc;
+system("cls");
+//valida si la opcion es un numero
+do{
+	cout<<"\nMENU:\n\n1- Utilizar Grafo Pre-guardado\n2- Crear Grafo\n3- Crear Matriz de Adyacencia\n4- Crear Matriz de Caminos\n5- Mostar Lista de Nodos Adyacencentes\n6- Recorrido En Profundidad\n7- Recorrido En Anchura\n8- Mostar Matriz de Floyd\n9- Mostar Matriz de Warshall\n10- Salir\n\n Ingrese la opcion: ";cin>>opUser;
+
+}while(!validaNum(opUser));
+
+opc=atoi(opUser);
 switch(opc){
 	case 1:
-         system("cls");
+        system("cls");
 		l.Insertarv(5);
 		l.Insertarv(8);
 		l.Insertarv(6);
@@ -19,7 +51,7 @@ switch(opc){
 		l.Insertarv(15);
 		l.creararco(5,6,4);
 		l.creararco(5,15,13);
-		l.creararco(5,19,2);
+ 		l.creararco(5,19,2);
 		l.creararco(15,8,5);
 		l.creararco(15,6,13);
 		l.creararco(8,19,21);
@@ -28,25 +60,43 @@ switch(opc){
 		l.creararco(6,11,5);
 		l.creararco(11,8,15);
 	break;
-	case 2:do{system("cls");cout<<"\nMENU:\n1- Nuevo Vertice\n2- Crear Arco\n\nIngrse la opcion: ";cin>>opc2;}while(opc2<1||opc>2);
+	case 2:
+	do{system("cls");cout<<"\nMENU:\n1- Nuevo Vertice\n2- Crear Arco\n\nIngrse la opcion: ";cin>>opc2;}while(opc2<1||opc>2);
 		switch(opc2){
 		case 1:system("cls");
+			char verticeUser[100], contUser[100];
 			if(opc==1){
 				l.BorrarListav();
-			}
+			} 
 			do{system("cls");cont=' ';
-                cout<<"\nIngrese el vertice: ";cin>>vertice;
+				do{
+					cout<<"\nIngrese el vertice: ";cin>>verticeUser;
+				}while(!validaNum(verticeUser));
+				vertice= atoi(verticeUser);
+                
                 l.Insertarv(vertice);
                 cout<<"\n\nDesea Ingresar Otro Vertice (S/N): ";cin>>cont;
 			}while(cont!='n'&&cont!='N');break;
 		break;
 		case 2:
+			char inicioUser[100], finUser[100], pesoUser[100];
 			do{
 				cont=' ';
 				system("cls");fflush(stdin);
-				cout<<"\nIngrse el vertice de inicio: ";cin>>inicio;
-				cout<<"\nIngrse el vertice de fin: ";cin>>final;
-				cout<<"\nIngrse el peso: ";cin>>peso;
+				do{
+					cout<<"\nIngrese el vertice de inicio: ";cin>>inicioUser;
+				}while(!validaNum(inicioUser));
+				inicio = atoi(inicioUser);
+				do{
+					cout<<"\nIngrse el vertice de fin: ";cin>>finUser;
+				}while(!validaNum(finUser));
+				final = atoi(finUser);
+				
+				do{
+					cout<<"\nIngrse el peso: ";cin>>pesoUser;	
+				}while(!validaNum(pesoUser));
+				peso = atoi(pesoUser);
+				
 				l.creararco(inicio,final,peso);
 				fflush(stdin);
 				cout<<"\n\nDesea crear otro arco (S/N): ";cin>>cont;
